88a2978266f1c4c34430e589836f832e024bf4e7
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-07-16 11:43:24 +0300

    feat: customers footer layout

--- src/main.js
+++ src/main.js
@@ -1,14 +1,32 @@
 const sidebarNavItems = document.querySelectorAll('.sidebar__nav-item')
+const paginationNavItems = document.querySelectorAll('.customers__pagination-btn')
 
-const clearSelectedStatus = () => {
+const clearSelectedSidebarStatus = () => {
   sidebarNavItems.forEach(item => {
     item.classList.remove('sidebar__nav-item--selected')
   })
 }
 
+const clearSelectedPaginationStatus = () => {
+  paginationNavItems.forEach(item => {
+    item.classList.remove('customers__pagination-btn--selected')
+  })
+}
+
 sidebarNavItems.forEach(item => {
   item.addEventListener('click', () => {
-    clearSelectedStatus();
+    clearSelectedSidebarStatus();
     item.classList.add('sidebar__nav-item--selected')
+  })
+})
+
+paginationNavItems.forEach(item => {
+  item.addEventListener('click', () => {
+    clearSelectedPaginationStatus();
+    item.classList.add('customers__pagination-btn--selected')
+
+    
+    const result = document.querySelector('.customers__pagination-btn--selected')
+    console.log(result.innerText)
   })
 })
