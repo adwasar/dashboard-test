c92e906455e4e65f523fd6bda8eec3dade97a44f
Vladislav <104913853+Adwasar@users.noreply.github.com>
2024-07-16 11:43:57 +0300

    --amend

--- src/main.js
+++ src/main.js
@@ -2,30 +2,29 @@ const sidebarNavItems = document.querySelectorAll('.sidebar__nav-item')
 const paginationNavItems = document.querySelectorAll('.customers__pagination-btn')
 
 const clearSelectedSidebarStatus = () => {
-  sidebarNavItems.forEach(item => {
+  sidebarNavItems.forEach((item) => {
     item.classList.remove('sidebar__nav-item--selected')
   })
 }
 
 const clearSelectedPaginationStatus = () => {
-  paginationNavItems.forEach(item => {
+  paginationNavItems.forEach((item) => {
     item.classList.remove('customers__pagination-btn--selected')
   })
 }
 
-sidebarNavItems.forEach(item => {
+sidebarNavItems.forEach((item) => {
   item.addEventListener('click', () => {
-    clearSelectedSidebarStatus();
+    clearSelectedSidebarStatus()
     item.classList.add('sidebar__nav-item--selected')
   })
 })
 
-paginationNavItems.forEach(item => {
+paginationNavItems.forEach((item) => {
   item.addEventListener('click', () => {
-    clearSelectedPaginationStatus();
+    clearSelectedPaginationStatus()
     item.classList.add('customers__pagination-btn--selected')
 
-    
     const result = document.querySelector('.customers__pagination-btn--selected')
     console.log(result.innerText)
   })
